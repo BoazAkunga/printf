44c65344654c307a3c109312cb56de6351aca075
BoazAkunga <boazakunga@gmail.com>
2023-10-16 15:38:04 -0700

    c-printf task 0 solution

--- _printf.c
+++ _printf.c
@@ -1,15 +1,42 @@
 #include "main.h"
 
 /**
-* _printf - function to produce the output according to format
-* @format: format according to which the arg will be printed
-* Return: No of chars to be printed
-*/
+ * write_character - Writes a single character to standard output.
+ * @c: The character to write.
+ * Return: The number of characters written.
+ */
+
+int write_character(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * write_string - Writes a string to standard output.
+ * @str: The string to write.
+ *
+ * Return: The number of characters written.
+ */
+
+int write_string(char *str)
+{
+	int str_len = 0;
+
+	while (str[str_len] != '\0')
+	str_len++;
+	return (write(1, str, str_len));
+}
+
+/**
+ * _printf - A custom printf-like function for formatted output.
+ * @format: The format string.
+ *
+ * Return: The number of characters written.
+ */
 
 int _printf(const char *format, ...)
 {
 	int char_print = 0;
-
 	va_list argsB;
 
 	if (format == NULL)
@@ -21,39 +48,30 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			char_print++;
-		}
-		else
+			char_print += write_character(*format);
+		} else
 		{
 			format++;
-			if (*format == '\0')
-				break;
-
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				char_print++;
-			}
-
+		if (*format == '\0')
+		{
+			break;
+		}
+		if (*format == '%')
+		{
+		char_print += write_character('%');
+		format++;
+		}
 			else if (*format == 'c')
 			{
 				char c = va_arg(argsB, int);
 
-				write(1, &c, 1);
-				char_print++;
+				char_print += write_character(c);
 			}
-
 			else if (*format == 's')
 			{
 				char *str = va_arg(argsB, char*);
-				int str_len = 0;
 
-			while (str[str_len] != '\0')
-				str_len++;
-
-				write(1, str, str_len);
-				char_print += str_len;
+				char_print += write_string(str);
 			}
 		}
 		format++;
@@ -62,3 +80,4 @@ int _printf(const char *format, ...)
 	va_end(argsB);
 	return (char_print);
 }
+
