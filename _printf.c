1f6bd44be55c167b1d7794b23aac92db9b5802a6
donmaleek <bamboloshari@gmail.com>
2023-10-17 18:20:12 -0700

    second amendment

--- _printf.c
+++ _printf.c
@@ -7,40 +7,43 @@
  */
 int _printf(const char *format, ...)
 {
-	int charac_print = 0;
-	char c;
 	va_list mylist_of_args;
+	char c;
+	int charac_print = 0;
+	int len = 0;
 
 	if (format == NULL)
 	return (-1);
 	va_start(mylist_of_args, format);
+	
 	while (*format)
 	{
 	if (*format != '%')
-	charac_print += write(1, format++, 1);
+	{
+	charac_print += write(1, format, 1); }
 	else
 	{
 	format++;
 	if (*format == '\0')
 	break;
 	if (*format == '%')
-	charac_print += write(1, format++, 1);
+	{
+	charac_print += write(1, format, 1); }
 	else if (*format == 'c')
+	{
 	c = va_arg(mylist_of_args, int);
-	charac_print += write(1, &c, 1);
-	if (*format == 's')
+	charac_print += write(1, &c, 1); }
+	else if (*format == 's')
 	{
 	char *str = va_arg(mylist_of_args, char*);
-	int len = 0;
 
 	while (str[len])
 	len++;
 	charac_print += write(1, str, len);
 	}
+	}
 	format++;
 	}
-	}
-
 	va_end(mylist_of_args);
 	return (charac_print);
 }
