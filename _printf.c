8d64f4d8c4be40134c6f8b7500f68b9e03f79042
BoazAkunga <boazakunga@gmail.com>
2023-10-17 18:16:18 -0700

    c-printf group project task solution

--- _printf.c
+++ _printf.c
@@ -16,29 +16,29 @@ int _printf(const char *format, ...)
 	va_start(mylist_of_args, format);
 	while (*format)
 	{
-	if (*format != '%')
-	charac_print += write(1, format++, 1);
-	else
-	{
-	format++;
-	if (*format == '\0')
-	break;
-	if (*format == '%')
-	charac_print += write(1, format++, 1);
-	else if (*format == 'c')
-	c = va_arg(mylist_of_args, int);
-	charac_print += write(1, &c, 1);
-	if (*format == 's')
-	{
-	char *str = va_arg(mylist_of_args, char*);
-	int len = 0;
+		if (*format != '%')
+			charac_print += write(1, format++, 1);
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+				charac_print += write(1, format++, 1);
+			else if (*format == 'c')
+				c = va_arg(mylist_of_args, int);
+			charac_print += write(1, &c, 1);
+			if (*format == 's')
+			{
+				char *str = va_arg(mylist_of_args, char*);
+				int len = 0;
 
-	while (str[len])
-	len++;
-	charac_print += write(1, str, len);
-	}
-	format++;
-	}
+				while (str[len])
+					len++;
+				charac_print += write(1, str, len);
+			}
+			format++;
+		}
 	}
 
 	va_end(mylist_of_args);
